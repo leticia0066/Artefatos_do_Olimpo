015ee617dc5c074925463e8a4c4b5731561a1311
leticia0066 <145374923+leticia0066@users.noreply.github.com>
2026-05-28 07:08:45 -0300

    adicionando a plataforma que se move

---
+++ ArtefatosDoOlimpo/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+
+    public float speed = 2f;
+
+    private Vector3 target;
+
+    void Start()
+    {
+        target = pointB.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            target,
+            speed * Time.deltaTime
+        );
+
+        if (Vector3.Distance(transform.position, target) < 0.1f)
+        {
+            if (target == pointA.position)
+            {
+                target = pointB.position;
+            }
+            else
+            {
+                target = pointA.position;
+            }
+        }
+    }
+}
